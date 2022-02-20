9161d804731f9312dfb8daa86e08e6ed0b313fd7
panvla <pandvlado@gmail.com>
2022-02-20 23:58:24 +0100

    mavep project created

---
+++ src/main/java/com/vladimirpandurov/springbootemployeeemanagement/SpringbootEmployeeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.springbootemployeeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEmployeeeManagementApplication.class, args);
+    }
+
+}
